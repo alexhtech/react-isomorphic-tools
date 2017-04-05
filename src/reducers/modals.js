997228235522330bdd5b667aab6cf3f6f6105d0f
epodgaetskiy <epodgaetskiy@gmail.com>
2017-04-05 10:35:39 +0300

    add reducer @@modals/closeAll, action closeAllModals

--- src/reducers/modals.js
+++ src/reducers/modals.js
@@ -4,10 +4,10 @@ const modals = (state = Immutable.Map({}), action)=> {
     switch (action.type) {
         case '@@modals/open':
             return state.set(action.meta, true)
-
         case '@@modals/close':
             return state.set(action.meta, false)
-
+        case '@@modals/closeAll':
+            return Immutable.Map({})
         default:
             return state
     }
